887242c6e77d5cb5f970589baf22e8d35f6b9b85
Wang Yu Ping <wang@jotform.com>
2014-01-04 15:54:45 +0800

    Added UI for creating form properties
    Added UI for creating form properties

--- example/src/com/jotform/jotformapisample/list/CreateFormPropertiesActivity.java
+++ example/src/com/jotform/jotformapisample/list/CreateFormPropertiesActivity.java
@@ -1,17 +1,59 @@
 package com.jotform.jotformapisample.list;
 
-import com.jotform.jotformapisample.R;
-
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+import com.jotform.api.JotformAPIClient;
+import com.jotform.jotformapisample.R;
+import com.jotform.jotformapisample.model.SharedData;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
 public class CreateFormPropertiesActivity extends Activity {
+	
+	private static final long FORM_ID = 0L;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 
-		setContentView(R.layout.activity_createquestion);
+		setContentView(R.layout.activity_createformproperty);
+		
+		initData();
+		initUI();
 	}
+	
+	private void initData() {
+		
+	}
+	
+	private void initUI() {
+		
+		Button createFormPropertiesButton = (Button) findViewById(R.id.button_createformproperty);
+		
+		createFormPropertiesButton.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+	}
+	
+	private void createFormProperties() {
+	
+		SharedData sharedData = (SharedData) getApplicationContext();
+		
+		JotformAPIClient apiClient = sharedData.getJotformAPIClient();
+		
+		apiClient.setFormProperties(FORM_ID, formProperties, new JsonHttpResponseHandler(){
+			
+		});
+	}
+	
 }
